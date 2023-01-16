0e8553518f5897c38f2a3a789354a6b9d27cb4a7
vtarasiuk <vladtarasuk03@gmail.com>
2023-01-16 23:55:04 +0200

    fix

--- CMakeFiles/main.dir/map.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/main.dir/map.cpp.o: /home/tarasyuk/Documents/MyProject/map.cpp \
- /usr/include/stdc-predef.h \
- /home/tarasyuk/Documents/MyProject/framework.h \
- /usr/include/c++/11/iostream
