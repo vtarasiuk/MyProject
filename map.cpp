ca6fb4649612d6e3973397a1cd3f0a2d7764fc57
vtarasiuk <vladtarasuk03@gmail.com>
2023-01-17 22:41:56 +0200

    add operator[]

--- map.cpp
+++ map.cpp
@@ -6,8 +6,28 @@ int Map::get_size(void)
   return this->size;
 }
 
-void Map::insert(int key, std::string value) {}
-void Map::insert(std::string key, int value) {}
+void Map::insert(std::string key, int value)
+{
+  if (!this->head)
+  {
+    this->head = new Node();
+    this->head->lpkey = key;
+    this->head->ivalue = value;
+    this->size++;
+    return;
+  }
+
+  Node* current = this->head;
+  while (current->next != nullptr)
+  {
+    current = current->next;
+  }
+  current->next = new Node();
+  current->next->lpkey = key;
+  current->next->ivalue = value;
+  this->size++;
+}
+
 void Map::insert(int key, int value)
 {
   if (!this->head)
@@ -30,33 +50,18 @@ void Map::insert(int key, int value)
   this->size++;
 }
 
-void Map::insert(std::string key, std::string value) {}
-
 void Map::print()
 {
   Node* current = this->head;
-  bool isKeyStr, isValStr;
+  bool isKeyStr;
   while (current != nullptr)
   {
     isKeyStr = (current->lpkey == "") ? false : true;
-    isValStr = (current->lpvalue == "") ? false : true; 
     
-    if (isKeyStr && isValStr)
+    if (isKeyStr)
     {
-      std::cout << "KEY: " << current->lpkey << std::endl;
-      std::cout << "VALUE: " << current->lpvalue << std::endl;
-      std::cout << "PNEXT: " << current->next << std::endl << std::endl;
-    }
-    else if (isKeyStr)
-    {
-      std::cout << "KEY: " << current->lpkey << std::endl;
+      std::cout << "KEY: " << current->lpkey << " (string)" << std::endl;
       std::cout << "VALUE: " << current->ivalue << std::endl;
-      std::cout << "PNEXT: " << current->next << std::endl << std::endl;
-    }
-    else if (isValStr)
-    {
-      std::cout << "KEY: " << current->ikey << std::endl;
-      std::cout << "VALUE: " << current->lpvalue << std::endl;
       std::cout << "PNEXT: " << current->next << std::endl << std::endl;
     }
     else
@@ -69,4 +74,28 @@ void Map::print()
     current = current->next;
   }
   std::cout << "SIZE: " << this->size << "\n\n";
+}
+
+int Map::operator[] (const int& key) const
+{
+  Node* current = this->head;
+  while (current != nullptr)
+  {
+    if (current->ikey == key) return current->ivalue;
+    current = current->next;
+  }
+  throw std::out_of_range("The key doesn't exist");
+  return 1;
+}
+
+int Map::operator[] (const std::string& key) const
+{
+  Node* current = this->head;
+  while (current != nullptr)
+  {
+    if (current->lpkey == key) return current->ivalue;
+    current = current->next;
+  }
+  throw std::out_of_range("The key doesn't exist");
+  return 1;
 }
